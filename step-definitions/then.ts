93038610865ed6cdd3a15e0aa8be4d84bfb14628
Prasanna Kumar <prasanna.kumar@headforwards.com>
2018-09-11 22:49:49 +0100

    split given when then files

--- step-definitions/then.ts
+++ step-definitions/then.ts
@@ -1,8 +1,14 @@
 import { SearchPageObject } from "../pages/searchPage";
 const { Then } = require("cucumber");
-
+/**
+ * Created by Prasanna Dommalapati on 11/09/2018.
+ */
 const search: SearchPageObject = new SearchPageObject();
 
 Then(/^I clear the search text$/, async () => {
-     await search.searchTextBox.clear();
+    await search.searchTextBox.clear();
+});
+
+Then(/^I click on google logo$/, async () => {
+    await search.logo.click();
 });
