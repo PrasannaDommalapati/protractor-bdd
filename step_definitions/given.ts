eda987651dbc46049073af39152ac5ed66612b5f
Prasanna Kumar <prasanna.kumar@headforwards.com>
2018-09-12 23:11:30 +0100

    test url

--- step_definitions/given.ts
+++ step_definitions/given.ts
@@ -2,8 +2,8 @@ import {defineSupportCode, Given} from 'cucumber';
 import {browser}                  from 'protractor';
 import {SearchPageObject}         from '../pages/searchPage';
 
-const chai                     = require('chai').use(require('chai-as-promised'));
-const expect                   = chai.expect;
+const chai = require('chai').use(require('chai-as-promised'));
+const expect = chai.expect;
 /**
  * Created by Prasanna Dommalapati on 11/09/2018.
  */
@@ -11,9 +11,17 @@ const search: SearchPageObject = new SearchPageObject();
 
 Given(/^I am on "(.*?)" search page$/, inSearchPage);
 
-function inSearchPage(text) {
+async function inSearchPage(text) {
 
     let actual = (text === 'google') ? 'Google' : text.concat(' - Google Search');
+    await getURL();
 
     return expect(browser.getTitle()).to.eventually.equal(actual);
+}
+
+function getURL() {
+    return Promise.resolve()
+        .then(() => browser.getCurrentUrl())
+        .then(console.log);
+
 }
