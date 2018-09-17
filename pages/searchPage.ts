76bebcf655993a309b374538d00deb8d9b5c7d6f
Prasanna Dommalapati <dommalapati.chowdary@gmail.com>
2018-09-17 16:25:59 +0100

    bdd working

--- pages/searchPage.ts
+++ pages/searchPage.ts
@@ -10,4 +10,5 @@ export class SearchPageObject {
         this.searchButton  = $('input[value=\'Google Search\']');
         this.logo          = $('div#logocont');
     }
+
 }
--- step_definitions/given.ts
+++ step_definitions/given.ts
@@ -1,19 +1,46 @@
-import {defineSupportCode, Given} from 'cucumber';
-import {browser}                  from 'protractor';
-import {SearchPageObject}         from '../pages/searchPage';
+import {Given}               from 'cucumber';
+import {browser, protractor} from 'protractor';
+import {LoginPageObject}     from '../pages/login';
 
-const chai = require('chai').use(require('chai-as-promised'));
-const expect = chai.expect;
+const chai                     = require('chai')
+    .use(require('chai-as-promised'));
+const expect                   = chai.expect;
 /**
  * Created by Prasanna Dommalapati on 11/09/2018.
  */
-const search: SearchPageObject = new SearchPageObject();
+const loginPO: LoginPageObject = new LoginPageObject();
 
 Given(/^I am on "(.*?)" search page$/, inSearchPage);
+Given(/^I navigate to "(.*?)"$/, navigateTo);
 
 function inSearchPage(text) {
 
-    let actual = (text === 'google') ? 'Google' : text.concat(' - Google Search');
+    let ExpectedTitle = (text === 'google') ? 'Google' : text.concat(' - Google Search');
 
-    return expect(browser.getTitle()).to.eventually.equal(actual);
+    return expect(browser.getTitle()).to.eventually.equal(ExpectedTitle);
 }
+
+function navigateTo(url) {
+
+    return browser.get(url);
+
+}
+
+// function loginDetails(credentials) {
+//
+//     return Promise.resolve()
+//                   .then(() => credentials.rawTable.forEach(login));
+//
+// }
+
+// function login(credentials) {
+//
+//      const username = credentials.shift();
+//      const password = credentials.shift();
+//
+//     return Promise.resolve()
+//                   .then(() => loginPO.password.sendKeys(password))
+//                   .then(() => loginPO.username.sendKeys(username))
+//                   .then(() => loginPO.password.sendKeys(protractor.Key.ENTER));
+//
+// }
